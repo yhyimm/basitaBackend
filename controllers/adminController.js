1d018f16431002c5d5ab0eb85337c93279a60be9
Yahya Elimam <126907506+yhyimm@users.noreply.github.com>
2024-10-11 22:43:15 +0300

    adding tokens

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,21 @@
 const Admin = require('../models/admin');  // Use PascalCase for model names
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// this key will be changed latter and stored in a .env file
+const key = 'd2f9c1a6b3e0e7d8f1a4b9c2f5e3b6a0d4c8e2a7f0b1d9c3f4e5a1b7d2c6f9e3';
 
 // Create a new admin
 const createAdmin = async (req, res) => {
     try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, key);
+
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+
+        console.log('token authenticated from createAdmin');
+
         const { username, password } = req.body;
 
         // Check if admin already exists
@@ -25,6 +37,7 @@ const createAdmin = async (req, res) => {
 const authenticateAdmin = async (req, res) => {
     try {
         const { username, password } = req.body;
+        console.log('username is: ',username);
 
         // Find the admin by username
         const admin = await Admin.findOne({ username });
@@ -39,17 +52,25 @@ const authenticateAdmin = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        res.status(200).json({ message: 'Admin authenticated' });
-        console.log('Admin authenticated');
-    }
-    catch (error) {
+        // Create a token
+        const token = jwt.sign({ adminId: admin._id }, key, { expiresIn: '1h' });
+        res.status(200).json({ token });
+        console.log('token is: ',token);
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
 
 const adminIndex = async (req, res) => {
     try {
-        // get all admins from the database and the plain text password
+
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, key);
+
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+
         const admins = await Admin.find();
         res.status(200).json(admins);
     } catch (error) {
@@ -59,32 +80,63 @@ const adminIndex = async (req, res) => {
 
 const deleteAdmin = async (req, res) => {
     try {
+        // authenticate the token
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, key);
+
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+
         const { username } = req.body;
 
         // Check if admin exists
-        const admin = await Admin.findOne
-        ({ username });
+        const admin = await Admin.findOne({ username });
         if (!admin) {
             return res.status(400).json({ message: 'Admin does not exist' });
         }
-        await Admin.deleteOne({ username});
+
+        await Admin.deleteOne({ username });
         res.status(200).json({ message: 'Admin deleted' });
         console.log('Admin deleted');
     }
+
     catch (error) {
         res.status(500).json({ message: error.message });
     }
+
 }
 
-const getAdmins = async (req, res) => {
-   try {
-      const admins = await Admin.find();
-      res.status(200).json(admins);
-   } catch (error) {
-      res.status(500).json({ message: error.message });
-   }
+const updateAdminPassword = async (req, res) => {
+    try {
+        
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, key);
+
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+
+        const { username, password } = req.body;
+
+        // Check if admin exists
+        const admin = await Admin.findOne({ username });
+        if (!admin) {
+            return res.status(400).json({ message: 'Admin does not exist' });
+        }
+
+        // Update password
+        admin.password = password;
+
+        await admin.save();
+        res.status(200).json({ message: 'Admin password updated' });
+        console.log('Admin password updated');
+    }
+
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
 }
 
-
-module.exports = { createAdmin,getAdmins, authenticateAdmin, adminIndex, deleteAdmin };  // Export the functions
-
+module.exports = { createAdmin, authenticateAdmin, adminIndex, deleteAdmin, updateAdminPassword };
--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,7 @@
 const projectModel = require('../models/projectModel');
 
-// we need to add token stuff later and also admin controllers
+const jwt = require('jsonwebtoken');
+const key = 'd2f9c1a6b3e0e7d8f1a4b9c2f5e3b6a0d4c8e2a7f0b1d9c3f4e5a1b7d2c6f9e3';
 
 const getProjects = (req, res) => {
     projectModel.find({lang:'EN'}).sort({ orderOfDisplay: 1 })
@@ -49,9 +50,67 @@ const getprojectByNameAr = (req, res) =>{
     });
 };
 
+const addProject = (req, res) => {
+
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, key);
+    
+    if (!decoded) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    const body = req.body;
+    
+    if (req.file) {
+        // make the body image as the name of the file
+        body.image = req.fileName;
+    }
+
+    body.order_of_display = parseInt(body.order_of_display);
+    const project = new ourProjects(body);
+    project.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ message: 'Error saving project', error: err });
+        });
+        project = new projectModel(body);
+
+}
+
+const updateProject = (req, res) => {
+    
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, key);
+    
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    
+        const id = req.params.id;
+        const body = req.body;
+    
+        if (req.file) {
+            body.image = req.fileName;
+        }
+    
+        body.order_of_display = parseInt(body.order_of_display);
+        projectModel.findByIdAndUpdate(id, body, { new: true })
+            .then(result => {
+                res.send(result);
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+
+
 module.exports = {
     getProjects,
     getProjectsAr,
     getProjectByName,
-    getprojectByNameAr
+    getprojectByNameAr,
+    addProject,
+    updateProject
 };
