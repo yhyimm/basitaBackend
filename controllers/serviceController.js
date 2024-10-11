b7c4951bc8c903f5b5df38b06ba670ae8ab2733d
Yahya Elimam <126907506+yhyimm@users.noreply.github.com>
2024-10-11 23:25:10 +0300

    continue adding tokens

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,6 +1,6 @@
 const serviceModel = require('../models/serviceModel');
-// we need to add token stuff later and also admin controllers
-
+const jwt = require('jsonwebtoken');
+const key = 'd2f9c1a6b3e0e7d8f1a4b9c2f5e3b6a0d4c8e2a7f0b1d9c3f4e5a1b7d2c6f9e3';
 
 const getServices = (req, res) => {
     serviceModel.find({lang:'EN'}).sort({ orderOfDisplay: 1 })
@@ -50,9 +50,56 @@ const getServiceByNameAr = (req, res) =>{
     });
 };
 
+
+const addService = (req, res) => {
+    
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, key);
+        
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    
+        const body = req.body;
+        const service = new serviceModel(body);
+    
+        service.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('error adding service');
+        });
+    };
+
+const updateService = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, key);
+
+    if (!decoded) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    const id = req.params.id;
+    const body = req.body;
+
+    serviceModel.findByIdAndUpdate(id, body, { new: true })
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('error updating service');
+        });
+};    
+
+
 module.exports = {
     getServices,
     getServicesAr,
     getServiceByName,
-    getServiceByNameAr
+    getServiceByNameAr,
+    addService,
+    updateService
 };
--- controllers/teamMemberController.js
+++ controllers/teamMemberController.js
@@ -1,5 +1,6 @@
 const memberModel = require('../models/team-memberModel')
-// we need to add token stuff later and also admin controllers
+const jwt = require('jsonwebtoken');
+const key = 'd2f9c1a6b3e0e7d8f1a4b9c2f5e3b6a0d4c8e2a7f0b1d9c3f4e5a1b7d2c6f9e3';
 
 const getMembers = (req, res) => {
     memberModel.find().sort({orderOfDisplay: 1})
@@ -12,6 +13,49 @@ const getMembers = (req, res) => {
     });
 };
 
+
+const addMember = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, key);
+
+    if (!decoded) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    const member = new memberModel(req.body);
+    member.save()
+    .then(result =>{
+        res.send(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).send('error adding member');
+    });
+};
+
+const updateMember = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, key);
+
+    if (!decoded) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    const id = req.params.id;
+    const body = req.body;
+
+    memberModel.findByIdAndUpdate(id, body)
+    .then(result =>{
+        res.send(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).send('error updating member');
+    });
+};
+
 module.exports = {
-    getMembers
+    getMembers,
+    addMember,
+    updateMember
 };
